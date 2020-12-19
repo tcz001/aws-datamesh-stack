11268245774e820e5c79f23a45e32a826f3db34b
tcz001 <fan.torchz@gmail.com>
2020-12-19 22:47:21 +0800

    add aws datapipeline

--- index.ts
+++ index.ts
@@ -6,7 +6,9 @@ import * as awsx from "@pulumi/awsx";
 const names = [];
 const input_bucket = new aws.s3.Bucket(`mesh-input-bucket`);
 const output_bucket = new aws.s3.Bucket(`mesh-output-bucket`);
+const pipeline = new aws.datapipeline.Pipeline("mesh-pipeline", {});
 names.push(input_bucket.id);
 names.push(output_bucket.id);
 // Export the name of the buckets
 export const bucketNames = names;
+export const pipelineNames = pipeline.id;
