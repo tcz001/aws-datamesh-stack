f253fa7c43026b52d9be19fa177f7f827089f9c3
tcz001 <fan.torchz@gmail.com>
2020-12-19 21:26:33 +0800

    change stack

--- index.ts
+++ index.ts
@@ -4,10 +4,9 @@ import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
 const names = [];
-for (let i = 0; i < 3; i++) {
-    const bucket = new aws.s3.Bucket(`my-bucket-${i}`);
-    names.push(bucket.id);
-}
-
+const input_bucket = new aws.s3.Bucket(`mesh-input-bucket-${i}`);
+const output_bucket = new aws.s3.Bucket(`mesh-output-bucket-${i}`);
+names.push(input_bucket.id);
+names.push(output_bucket.id);
 // Export the name of the buckets
 export const bucketNames = names;
