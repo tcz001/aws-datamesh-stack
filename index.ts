31d63462a286fa07f04f4ac0a56a51cbefcab8e9
tcz001 <fan.torchz@gmail.com>
2020-12-19 23:50:29 +0800

    change config struct to support multiple instance

--- index.ts
+++ index.ts
@@ -2,13 +2,33 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const names = [];
-const input_bucket = new aws.s3.Bucket(`mesh-input-bucket`);
-const output_bucket = new aws.s3.Bucket(`mesh-output-bucket`);
-const pipeline = new aws.datapipeline.Pipeline("mesh-pipeline", {});
-names.push(input_bucket.id);
+// Create an AWS resource
+const input_ports = [];
+const output_ports = [];
+const pipelines = [];
+interface Config {
+    type: string;
+    count: number;
+}
+
+let config = new pulumi.Config();
+let input_config = config.requireObject<Config>("input");
+let output_config = config.requireObject<Config>("output");
+let pipeline_config = config.requireObject<Config>("pipeline");
+for (let i = 0; i < input_config.count; i++){
+   const input_bucket = new aws.s3.Bucket(`mesh-input-bucket-${i}`);
+   input_ports.push(input_bucket.id);
+}
+for (let i = 0; i < output_config.count; i++){
+   const output_bucket = new aws.s3.Bucket(`mesh-output-bucket-${i}`);
+   output_ports.push(input_bucket.id);
+}
+for (let i = 0; i < pipeline_config.count; i++){
+   const pipeline = new aws.datapipeline.Pipeline(`mesh-pipeline-${i}`, {});
+   pipelines.push(input_bucket.id);
+}
 names.push(output_bucket.id);
 // Export the name of the buckets
-export const bucketNames = names;
-export const pipelineNames = pipeline.id;
+export const inputPortsIds = input_ports;
+export const outputPortsIds = output_ports;
+export const pipelineIds = pipelines;
